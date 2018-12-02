f31186cc9a68027b5a42a5263ce955bbe5ed5241
Skarrg <orestes.moiran@gmail.com>
2018-12-01 18:47:06 -0800

    Phew all of it in

--- liri.js
+++ liri.js
@@ -18,7 +18,7 @@ var search = process.argv.slice(3).join("+");
 
 switch (command) {
 
-    case "spotify-song":
+    case "spotify":
         if (search) {
             spotifySearch(search);
         } else {
@@ -46,6 +46,10 @@ switch (command) {
         }
         break;
 
+    case "dowhatitsays":
+        doWhatItSays();
+        break;
+
     default:
         console.log("My current commands are: spotify, movie, concert");
         break;
@@ -59,11 +63,13 @@ function spotifySearch(song) {
         } else {
             for (var i = 0; i < data.tracks.items.length; i++) {
                 var songData = data.tracks.items[i];
-                console.log("Artist: " + songData.artists[0].name);
-                console.log("Song: " + songData.name);
-                console.log("URL: " + songData.preview_url);
-                console.log("Album: " + songData.album.name);
-                console.log("*************");
+                var summary = "\nArtist: " + songData.artists[0].name
+                    + "\nSong: " + songData.name
+                    + "\nURL: " + songData.preview_url
+                    + "\nAlbum: " + songData.album.name
+                    + "\n*************";
+
+                console.log(summary);
             }
 
         }
@@ -72,25 +78,57 @@ function spotifySearch(song) {
 }
 
 function movieSearch(movie) {
-    console.log(movie);
-    axios.get("http://www.omdbapi.com/?t=" + movie
-        + "&y=&plot=short&apikey=trilogy").then(
-            function (response) {
+    axios.get("http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy").then(
+        function (response) {
 
-                summary = "\nRelease Date: " + response.data.Released +
-                    "\nImdb Rating: " + response.data.imdbRating +
-                    "\nRotten Tomatoes Rating: " + response.data.Ratings[1].Value +
-                    "\nCountry of Origin: " + response.data.Country +
-                    "\nLanguage: " + response.data.Language +
-                    "\nPlot: " + response.data.Plot +
-                    "\nActors: " + response.data.Actors;
+            summary = "\nRelease Date: " + response.data.Released +
+                "\nImdb Rating: " + response.data.imdbRating +
+                "\nRotten Tomatoes Rating: " + response.data.Ratings[1].Value +
+                "\nCountry of Origin: " + response.data.Country +
+                "\nLanguage: " + response.data.Language +
+                "\nPlot: " + response.data.Plot +
+                "\nActors: " + response.data.Actors;
+            console.log("*************");
 
-                console.log(summary);
-            }
-        );
+            console.log(summary);
+        }
+    );
 }
 
 
-function concertSearch(concert) {
-    console.log("concert blank")
+function concertSearch(artist) {
+    var queryUrl = 'https://rest.bandsintown.com/artists/' + artist + '/events?app_id=codingbootcamp'
+
+    request(queryUrl, function (error, response, body) {
+
+        let results = JSON.parse(body);
+
+        if (results.length === 0) {
+            console.log("No such band found. Try again.")
+        }
+
+        if (error) {
+            console.log('Error: ', error)
+        };
+
+        results.forEach(function (event) {
+            let name = event.venue.name;
+            let city = event.venue.city;
+            let date = moment(event.datetime).format("MM/DD/YYYY");
+            let country = event.venue.country;
+            summary = "\nDate: " + date +
+                "\nVenue: " + name +
+                "\nLocation: " + city + ", " + country +
+                "\n*************";
+
+            console.log(summary);
+        });
+    });
+};
+
+function doWhatItSays() {
+    fs.readFile("random.txt", "utf-8", function (err, data){
+        if (err) { throw err }
+        spotifySearch(data);
+    })
 }
